720e22930897e180d07e3898cc3767b5b8c590c1
ramzihasanov <bd54tkjwr@code.edu.az>
2024-01-15 19:35:30 +0400

    asd

--- Solution1/src/FinalExc/Areas/manage/Controllers/DashBoardController.cs
+++ Solution1/src/FinalExc/Areas/manage/Controllers/DashBoardController.cs
@@ -1,11 +1,11 @@
 ﻿using FinalExp.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-
 namespace FinalExc.Areas.manage.Controllers
 {
     [Area("manage")]
+    [Authorize(Roles = "Admin")]
     public class DashBoardController : Controller
     {
         private readonly UserManager<AppUser> userManager;
@@ -21,32 +21,32 @@ namespace FinalExc.Areas.manage.Controllers
             return View();
         }
 
-        public async Task<IActionResult> CreateAdmin()
-        {
-            AppUser user = new AppUser()
-            {
-                Fullname = "Admin",
-                UserName="Admin111"
-            };
-           await userManager.CreateAsync(user, "Admin111");
-           return View();
-        }
-        public async Task<IActionResult> CreateRoll()
-        {
-            IdentityRole rol1 = new IdentityRole("Admin");
-            IdentityRole rol2 = new IdentityRole("user");
+        //public async Task<IActionResult> CreateAdmin()
+        //{
+        //    AppUser user = new AppUser()
+        //    {
+        //        Fullname = "Admin",
+        //        UserName="Admin111"
+        //    };
+        //    var result=await userManager.CreateAsync(user, "Admin111@");
+        //   return Ok(result);
+        //}
+        //public async Task<IActionResult> CreateRoll()
+        //{
+        //    IdentityRole rol1 = new IdentityRole("Admin");
+        //    IdentityRole rol2 = new IdentityRole("user");
 
-            await roleManager.CreateAsync(rol1);
-            await roleManager.CreateAsync(rol2);
-            return View();
+        //    await roleManager.CreateAsync(rol1);
+        //    await roleManager.CreateAsync(rol2);
+        //    return Ok();
 
-        }
-        public async Task<IActionResult> AddRoll()
-        {
-            var appuser = await userManager.FindByNameAsync("Admin111");
-            if(appuser != null)
-            await userManager.AddToRoleAsync(appuser, "Admin");
-            return View();
-        }
+        //}
+        //public async Task<IActionResult> AddRoll()
+        //{
+        //    var appuser = await userManager.FindByNameAsync("Admin111");
+        //    if(appuser != null)
+        //    await userManager.AddToRoleAsync(appuser, "Admin");
+        //    return Ok();
+        //}
     }
 }
